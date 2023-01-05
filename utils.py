98a918d511c1820dfa4a34e8cac2807f054b4568
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-01-05 15:49:41 -0300

    date utils functions

---
+++ utils.py
@@ -0,0 +1,23 @@
+
+from datetime import datetime, timedelta, date
+from typing import Dict
+
+def day_report_time(days_ago: int) -> Dict[str, str]:
+    today = date.today()
+    old_date = today - timedelta(days=days_ago)
+    return {"startDate": str(today), "endDate": str(old_date)}
+
+
+def month_report_time(months_ago: int) -> Dict[str, str]:
+    today = date.today()
+    old_date = today - timedelta(months_ago * 30)
+    return {"startDate": str(today), "endDate": str(old_date)}
+
+
+def year_report_time(years_ago: int) -> Dict[str, str]:
+    if years_ago > 1:
+        print("We cannot retrieve more than 1 year")
+        return
+    today = date.today()
+    old_date = today - timedelta(days=years_ago * 365)
+    return {"startDate": str(today), "endDate": str(old_date)}
