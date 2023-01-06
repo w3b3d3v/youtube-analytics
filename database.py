80b8356daf748e7dc59824278d5d6827ff14b90c
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-01-06 08:59:59 -0300

    adjusting database to support the other request, new table

--- database.py
+++ database.py
@@ -16,6 +16,31 @@ class Database:
             print(e)
             return
 
+    def create_videos_table(self, conn):
+        table_sql = """CREATE TABLE IF NOT EXISTS videos (id integer PRIMARY KEY, video_id TEXT, views INTEGER, likes INTEGER, comments INTEGER, averageViewDuration INTEGER, estimatedMinutesWatched INTEGER, title TEXT, description TEXT);"""
+        if not conn:
+            return False
+        try:
+            c = conn.cursor()
+            c.execute(table_sql)
+            return True
+        except Error as e:
+            print(e)
+            return False
+    
+    def insert_videos_data(self, conn, data):
+        insert_sql = """INSERT INTO videos (video_id, views, likes, comments, averageViewDuration, estimatedMinutesWatched, title, description) VALUES (?, ?, ?, ?, ?, ?, ?, ?);"""
+        
+        try:
+            c = conn.cursor()
+            c.execute(insert_sql, data)
+            conn.commit()
+            return c.lastrowid
+
+        except Error as e:
+            print(e)
+            return False
+
     def create_analytics_table(self, conn):
         table_sql = """CREATE TABLE IF NOT EXISTS analytics (id integer PRIMARY KEY, date TEXT, estimatedMinutesWatched INTEGER, views INTEGER, likes INTEGER, subscribersGained INTEGER, comments INTEGER, averageViewDuration INTEGER);"""
         if not conn:
