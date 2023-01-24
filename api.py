102629ad858e2af20765fa897396eda3b123ee54
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-01-24 07:48:26 -0300

    updating API urls

--- api.py
+++ api.py
@@ -15,30 +15,29 @@ POST_HEADERS = {
 
 class Playlist:
     def get_all(self):
-        req = requests.get(url="http://localhost:1337/api/playlists?populate=*", headers=HEADERS)
+        req = requests.get(url="https://web3dev.uc.r.appspot.com/api/playlists?populate=*", headers=HEADERS)
         return req.text
     
     def get_by_id(self, playlist_id):
-        req = requests.get(url=f"http://localhost:1337/api/playlist/{playlist_id}?populate=*", headers=HEADERS)
+        req = requests.get(url=f"https://web3dev.uc.r.appspot.com/api/playlists/{playlist_id}?populate=*", headers=HEADERS)
         return json.dumps(req.text)
     
     def insert(self, playlist_data):
         json_data = json.dumps(playlist_data)
-       
-        res = requests.post("http://localhost:1337/api/playlists", headers=POST_HEADERS, data=json_data)
+        res = requests.post("https://web3dev.uc.r.appspot.com/api/playlists", headers=POST_HEADERS, data=json_data)
         return res.text
 
 class Video:
     def get_all(self):
-        req = requests.get(url="http://localhost:1337/api/videos?populate=*", headers=HEADERS)
+        req = requests.get(url="https://web3dev.uc.r.appspot.com/api/videos?populate=*", headers=HEADERS)
         return req.text.json()
-    
+
     def get_by_id(self, video_id):
-        req = requests.get(url=f"http://localhost:1337/api/playlist/{video_id}?populate=*", headers=HEADERS)
+        req = requests.get(url=f"https://web3dev.uc.r.appspot.com/api/videos/{video_id}?populate=*", headers=HEADERS)
         return req.text
-    
+
     def insert(self, video_data):
         json_data = json.dumps(video_data)
-        res = requests.post("http://localhost:1337/api/videos", headers=POST_HEADERS, data=json_data, params={"populate":"playlists"})
+        res = requests.post("https://web3dev.uc.r.appspot.com/api/videos", headers=POST_HEADERS, data=json_data, params={"populate":"playlists"})
         return res.text
-    
+
