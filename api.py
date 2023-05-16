48da1030e4a63c5dfdb33fa18f92ab71b384396f
Daniel Cukier <danicuki@gmail.com>
2023-05-16 14:59:20 -0300

    added API URL env

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ import os
 load_dotenv()
 
 STRAPI_API_TOKEN = os.getenv("STRAPI_API_TOKEN")
+API_URL= "https://strapi."
 HEADERS = {
     "Authorization": f"bearer {STRAPI_API_TOKEN}"
 }
@@ -15,28 +16,28 @@ POST_HEADERS = {
 
 class Playlist:
     def get_all(self):
-        req = requests.get(url="https://web3dev.uc.r.appspot.com/api/playlists?populate=*", headers=HEADERS)
+        req = requests.get(url=os.getenv("API_URL") + "playlists?populate=*", headers=HEADERS)
         return req.text
     
     def get_by_id(self, playlist_id):
-        req = requests.get(url=f"https://web3dev.uc.r.appspot.com/api/playlists/{playlist_id}?populate=*", headers=HEADERS)
+        req = requests.get(url=os.getenv("API_URL") + "playlists/{playlist_id}?populate=*", headers=HEADERS)
         return json.dumps(req.text)
     
     def insert(self, playlist_data):
         json_data = json.dumps(playlist_data)
-        res = requests.post("https://web3dev.uc.r.appspot.com/api/playlists/createOrUpdate", headers=POST_HEADERS, data=json_data)
+        res = requests.post(os.getenv("API_URL") + "playlists/createOrUpdate", headers=POST_HEADERS, data=json_data)
         return res.text
 
 class Video:
     def get_all(self):
-        req = requests.get(url="https://web3dev.uc.r.appspot.com/api/videos?populate=*", headers=HEADERS)
+        req = requests.get(url=os.getenv("API_URL") + "videos?populate=*", headers=HEADERS)
         return req.text.json()
 
     def get_by_id(self, video_id):
-        req = requests.get(url=f"https://web3dev.uc.r.appspot.com/api/videos/{video_id}?populate=*", headers=HEADERS)
+        req = requests.get(url=os.getenv("API_URL") + "videos/{video_id}?populate=*", headers=HEADERS)
         return req.text
 
     def insert(self, video_data):
         json_data = json.dumps(video_data)
-        res = requests.post("https://web3dev.uc.r.appspot.com/api/videos/createOrUpdate", headers=POST_HEADERS, data=json_data, params={"populate":"playlists"})
+        res = requests.post(os.getenv("API_URL") + "videos/createOrUpdate", headers=POST_HEADERS, data=json_data, params={"populate":"playlists"})
         return res.text
